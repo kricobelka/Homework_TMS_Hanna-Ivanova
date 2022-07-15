fbc2d1db6a8d70010f6ea51ffd2a3e05cd11e698
Hanna Ivanova <hijikataanya@gmail.com>
2022-07-15 17:33:05 +0300

    Homework 2 has been added

---
+++ Homework2fact/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework2fact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert the number you would like to factorial");
+            int n = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            Console.WriteLine(factorial);
+                }
+            Console.ReadKey();
+            }
+        }
+    }
