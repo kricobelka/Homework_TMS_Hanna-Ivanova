ca5bbfd47253d13c583cb860f0dd062cb37deafc
Hanna Ivanova <hijikataanya@gmail.com>
2022-07-17 16:34:04 +0300

    Calculator has been created

--- Homework3calc/Program.cs
+++ Homework3calc/Program.cs
@@ -6,7 +6,77 @@ namespace Homework3calc
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please type the first number");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please type the second number");
+            int number2 = int.Parse(Console.ReadLine());
+            {
+                //char operation = char.Parse(Console.ReadLine());
+                Console.WriteLine("Choose an operation to receive a result:" + Environment.NewLine +
+                    "\tp - Plus" + Environment.NewLine +
+                    "\td - Divide" + Environment.NewLine +
+                    "\tm - Multiply" + Environment.NewLine +
+                    "\tmin - Minus" + Environment.NewLine +
+                    "\tc - Clear (AC)" + Environment.NewLine +
+                    "\tr - Remainder (%)");
+                switch (Console.ReadLine())
+                {
+                    case "a":
+                        {
+                            Console.WriteLine($"The result is {number1} + {number2} = {number1 + number2}");
+                            break;
+                        }
+                    case "d":
+                        {
+                            if (number2 != 0)
+                            {
+                                Console.WriteLine($"The result is {number1} / {number2} = {number1 / number2}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error. Please insert corretc second number.");
+                            }
+                                number2 = int.Parse (Console.ReadLine());
+                                if (number2 != 0)
+                                {
+                                    Console.WriteLine($"The result is {number1} / {number2} = {number1 / number2}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error. Please insert corretc second number.");
+                                    Console.ReadLine();
+                                }
+                                break;
+                        }
+                    case "m":
+                        {
+                            Console.WriteLine($"The result is {number1} * {number2} = {number1 * number2}");
+                            break;
+                        }   
+                    case "min":
+                        {
+                            Console.WriteLine($"The result is {number1} - {number2} = {number1 - number2}");
+                            break;
+                        }
+                    case "r":
+                        {
+                            Console.WriteLine($"The result is {number1} % {number2} = {number1 % number2}");
+                            break;
+                        }
+                    //case "c":
+                    //    Console.WriteLine("");
+                    //    Console.WriteLine("Please insert new numbers");
+                    //    number1 = int.Parse(Console.ReadLine());
+                    //    number2 = int.Parse(Console.ReadLine());
+                    default:
+                        {
+                            Console.WriteLine("Wrong option!");
+                            break;
+                        }
+                }
+            }
+                    Console.ReadKey();
         }
     }
 }
+
