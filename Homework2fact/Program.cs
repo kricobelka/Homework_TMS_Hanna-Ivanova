17c5d59a8b068c3ac59e25340bbf03576e48386b
Hanna Ivanova <hijikataanya@gmail.com>
2022-07-17 13:27:00 +0300

    Adding new files

--- Homework2fact/Program.cs
+++ Homework2fact/Program.cs
@@ -9,13 +9,13 @@ namespace Homework2fact
             Console.WriteLine("Please insert the number you would like to factorial");
             int n = int.Parse(Console.ReadLine());
             int factorial = 1;
-            
+
             for (int i = 1; i <= n; i++)
             {
                 factorial = factorial * i;
-            Console.WriteLine(factorial);
-                }
-            Console.ReadKey();
+                Console.WriteLine(factorial);
             }
+            Console.ReadKey();
         }
     }
+}
---
+++ Homework3calc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework3calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
