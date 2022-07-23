b6ca7a0080320ce9ae87bac6bf918ebe7261f60c
Hanna Ivanova <hijikataanya@gmail.com>
2022-07-23 20:37:38 +0300

    Addign stack homework4

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Stack
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[3] { 3, 7, 18 };
+            Stack<int> stack = new Stack<int>(array);
+            foreach (int el in stack)
+            {
+                Console.WriteLine($"The stack includes the number" + " " + el);
+            }
         }
     }
 }
