141e41a7b11ced84ac544375f3240e9402f0065d
Hanna Ivanova <hijikataanya@gmail.com>
2022-07-23 20:27:09 +0300

    Homework 3 arrays

--- Homework3calc/Program.cs
+++ Homework3calc/Program.cs
@@ -6,76 +6,77 @@ namespace Homework3calc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please type the first number");
-            int number1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please type the second number");
-            int number2 = int.Parse(Console.ReadLine());
+
             //int i = 0;
-            // while (i != -1)
-            Console.WriteLine("Choose an operation to receive a result:" + Environment.NewLine +
+            string operation = "";
+            while (operation != "e")
+            {
+                Console.WriteLine("Please type the first number");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please type the second number");
+                int number2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Choose an operation to receive a result:" + Environment.NewLine +
                 "\tp - Plus" + Environment.NewLine +
                 "\td - Divide" + Environment.NewLine +
                 "\tm - Multiply" + Environment.NewLine +
                 "\tmin - Minus" + Environment.NewLine +
-                //"\tc - Clear (AC)" + Environment.NewLine +
+                "\tc - Clear (AC)" + Environment.NewLine +
                 "\tr - Remainder (%)");
-            //string operation = Console.ReadLine();
-           // switch (operation)
-           switch (Console.ReadLine())
-            {
-                case "a":
-                    {
-                        Console.WriteLine($"The result is {number1} + {number2} = {number1 + number2}");
-                        break;
-                    }
-                case "d":
-                    {
-                        if (number2 != 0)
+                operation = Console.ReadLine();
+                // switch (operation)
+                switch (operation)
+                {
+                    case "a":
                         {
-                            Console.WriteLine($"The result is {number1} / {number2} = {number1 / number2}");
+                            Console.WriteLine($"The result is {number1} + {number2} = {number1 + number2}");
+                            break;
                         }
-                        else
+                    case "d":
                         {
-                            Console.WriteLine("Error. Please insert corretc second number.");
+                            if (number2 != 0)
+                            {
+                                Console.WriteLine($"The result is {number1} / {number2} = {number1 / number2}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error. Please insert corretc second number.");
+                            }
+                            number2 = int.Parse(Console.ReadLine());
+                            if (number2 != 0)
+                            {
+                                Console.WriteLine($"The result is {number1} / {number2} = {number1 / number2}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error. Please insert corretc second number.");
+                                Console.ReadLine();
+                            }
+                            break;
                         }
-                        number2 = int.Parse(Console.ReadLine());
-                        if (number2 != 0)
+                    case "m":
                         {
-                            Console.WriteLine($"The result is {number1} / {number2} = {number1 / number2}");
+                            Console.WriteLine($"The result is {number1} * {number2} = {number1 * number2}");
+                            break;
                         }
-                        else
+                    case "min":
                         {
-                            Console.WriteLine("Error. Please insert corretc second number.");
-                            Console.ReadLine();
+                            Console.WriteLine($"The result is {number1} - {number2} = {number1 - number2}");
+                            break;
                         }
+                    case "r":
+                        {
+                            Console.WriteLine($"The result is {number1} % {number2} = {number1 % number2}");
+                            break;
+                        }
+                    case "c":
                         break;
-                    }
-                case "m":
-                    {
-                        Console.WriteLine($"The result is {number1} * {number2} = {number1 * number2}");
-                        break;
-                    }
-                case "min":
-                    {
-                        Console.WriteLine($"The result is {number1} - {number2} = {number1 - number2}");
-                        break;
-                    }
-                case "r":
-                    {
-                        Console.WriteLine($"The result is {number1} % {number2} = {number1 % number2}");
-                        break;
-                    }
-                //case "c":
-                //    Console.WriteLine("");
-                //    Console.WriteLine("Please insert new numbers");
-                //    number1 = int.Parse(Console.ReadLine());
-                //    number2 = int.Parse(Console.ReadLine());
-                default:
-                    {
-                        Console.WriteLine("Wrong option!");
-                        break;
-                    }
-                    Console.ReadKey();
+                    default:
+                        {
+                            Console.WriteLine("Wrong option!");
+                            break;
+                        }
+                }
+            
             }
         }
     }
---
+++ Homework4arrays/Homework4arrays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework4arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert the number of elements");
+            int N = int.Parse(Console.ReadLine());
+            int[] array = new int[N];
+            int sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("PLease insert elements of the array");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum += array[i];
+                }
+            }
+            Console.WriteLine($"sum of the odd elements: {sum}");
+            ////min e
+            // first way to find minimum value from the array:
+            var min = array.Min();
+            Console.WriteLine(min);
+
+            //second way to find minimum value from the array:
+            int min2 = array[0];
+                for (int i = 0; i < N; i++)
+                {
+                    if (array[0] < min)
+                    {
+                        min2 = array[0];
+                    }
+                }
+                Console.WriteLine(min2);
+            Console.ReadKey();
+        }  
+        }
+        }
+
