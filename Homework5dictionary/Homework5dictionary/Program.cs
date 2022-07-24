1feab3fe6d8b3c7ef8b1402770de8714bb67af64
Hanna Ivanova <hijikataanya@gmail.com>
2022-07-25 00:50:35 +0300

    Dictionary switch-case 1

--- Homework5dictionary/Homework5dictionary/Program.cs
+++ Homework5dictionary/Homework5dictionary/Program.cs
@@ -6,7 +6,7 @@ namespace Homework5dictionary
     {
         static void Main(string[] args)
         {
-            Dictionary <string, int> SurnameAndMarks = new Dictionary<string, int>()
+            Dictionary<string, int> SurnameAndMarks = new Dictionary<string, int>()
             {
                 {"Buel", 9},
                 {"Belova", 8},
@@ -16,42 +16,46 @@ namespace Homework5dictionary
             string surname;
             int mark;
             //while (operation > 0 && operation < 9)
-            //{
+            {
                 Console.WriteLine("Choose an option listed below" + Environment.NewLine +
                 "\t1: Add mark of the pupil" + Environment.NewLine +
                 "\t2: Change mark of the pupil" + Environment.NewLine +
                 "\t3: Delete mark of the pupil" + Environment.NewLine +
                 "\t4: Output marks and surnames of pupils" + Environment.NewLine +
                 "\t5: Output average mark of the pupils" + Environment.NewLine +
-                "\t6: Output pupils with the best mark/t" + Environment.NewLine +
-                "\t7:Output pupils with the mark equal to or more than 8/t" + Environment.NewLine +
-                "\t8: Output pupils with the mark equal to or less than 8/t");
-               
-            operation = int.Parse (Console.ReadLine());
-            foreach (KeyValuePair<string, int> surnameAndMark in SurnameAndMarks)
-            {
-                switch (operation)
-                {
-                    case 1:
+                "\t6: Output pupils with the best mark" + Environment.NewLine +
+                "\t7:Output pupils with the mark equal to or more than 8" + Environment.NewLine +
+                "\t8: Output pupils with the mark equal to or less than 8");
 
-                        surname = Console.ReadLine();
-                        mark = int.Parse (Console.ReadLine());
-                        if (SurnameAndMarks.ContainsKey(surname) && SurnameAndMarks.ContainsValue (mark))
-                        {
-                            surname = surnameAndMark.Key;
-                            mark = surnameAndMark.Value;
-                            Console.WriteLine("This pupil is in the list");
-                        }
-                        else 
-                        {
-                            SurnameAndMarks.Add(surname, mark);
-                        }
-                            Console.WriteLine($"key:{surnameAndMark.Key}-value:{surnameAndMark.Value}");
+                operation = int.Parse(Console.ReadLine());
+                foreach (KeyValuePair<string, int> surnameAndMark in SurnameAndMarks)
+                {
+                    //почему выводит только первое значение в словаре? Console.WriteLine($"key:{surnameAndMark.Key}-value:{surnameAndMark.Value}");
+                    switch (operation)
+                    {
+                        case 1:
+
+                            surname = Console.ReadLine();
+                            mark = int.Parse(Console.ReadLine());
+                            if (SurnameAndMarks.ContainsKey(surname) && SurnameAndMarks.ContainsValue(mark))
+                            {
+                                surname = surnameAndMark.Key;
+                                mark = surnameAndMark.Value;
+                                Console.WriteLine("This pupil is in the list");
+                            }
+                            else
+                            {
+                                SurnameAndMarks.Add(surname, mark);
+                                Console.WriteLine($"key:{surname}-value:{mark} has been added");
+                            }
                             break;
-                        }
+                    }
+                    // вывести всех:
+                    // почему выводит только первое значение в словаре? Console.WriteLine(surnameAndMark);
                 }
 
             }
         }
     }
+}
 
