db73644bc15c757d92f814ba168f44f35c37d525
Hanna Ivanova <hijikataanya@gmail.com>
2022-07-29 00:26:16 +0300

    Homework 6 class Iphone

---
+++ HomeworkIphoneclass/HomeworkIphoneclass/Iphone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkIphoneclass
+{
+    public class Iphone
+    {
+        private string number;
+        private string model;
+        private double _weight;
+        public Iphone(string number, string model, double weight)
+        {
+            Number = number;
+            Model = model;
+            _weight = weight;
+        }
+        public string Number
+        {
+            get;
+            set;
+        }
+        public string Model
+        {
+            get;
+            set;
+        }
+        public double Weight
+        {
+            get
+            { 
+                return _weight; 
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _weight = 0;
+                    Console.WriteLine("Wrong weight");
+                }
+            else
+                {
+                    _weight = value;
+                }
+            }
+        }
+        public void ReceiveCall(string nameofcaller)
+        {
+            Console.WriteLine("Name of caller is");
+            nameofcaller = Console.ReadLine();
+            Console.WriteLine($"{nameofcaller} is calling you");
+        }
+    }
+}
---
+++ HomeworkIphoneclass/HomeworkIphoneclass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeworkIphoneclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Iphone iphone = new Iphone("38337", "Iphone 12", 250d);
+            Console.WriteLine(iphone);
+            Console.WriteLine($"number: {iphone.Number}, model: {iphone.Model}, weight: {iphone.Weight}");
+            iphone.Weight = -7;
+            Console.WriteLine(iphone.Weight);
+            iphone.ReceiveCall("nameofcaller");
+
+        }
+    }
+}
