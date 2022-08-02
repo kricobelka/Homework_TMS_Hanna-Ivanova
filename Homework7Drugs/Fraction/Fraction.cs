b64303610dc98558425df41e092e91b6587f88b8
Hanna Ivanova <hijikataanya@gmail.com>
2022-08-03 00:19:27 +0300

    Fraction

---
+++ Homework7Drugs/Fraction/Fraction.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fraction
+{
+    internal class Fraction
+    {
+        //private double _numerator;
+        private double _denumerator;
+
+        public Fraction(int numerator, int denumerator)
+        {
+            Numerator = numerator;
+            Denumerator = denumerator;
+        }
+
+        public int Numerator
+        {
+            get; private set;
+        }
+
+        public int Denumerator
+        {
+            //переносить ли на след строку return и в каких случаях?
+            get { return (int)_denumerator; }
+            set
+            {
+                if (value < 0)
+                {
+                    _denumerator = 1;
+                    Console.WriteLine("Denumerator must be positive");
+                }
+                else
+                {
+                    _denumerator = value;
+                }
+            }
+        }
+        public int Addition(Fraction fraction1)
+        {
+            return Numerator + Denumerator;
+        }
+        // почему все время корректно возвращает, даже если название переменной fraction не совпадает?
+        public int Subtraction(Fraction fractio)
+        {
+            if (Numerator < Denumerator)
+            {
+                return Denumerator - Numerator;
+            }
+            else if (Numerator > Denumerator)
+            {
+                return Numerator - Denumerator;
+            }
+            else
+            {
+                return Numerator - Denumerator;
+            }
+        }
+        public double Division(Fraction fraction1)
+        {
+            return (double)Numerator / Denumerator;
+        }
+        public int Multiplication(Fraction fraction1)
+        {
+            return Numerator * Denumerator;
+        }
+        //можно и след образом:
+        //public int Multiplication ()
+        //{
+        //    return Numerator * Denumerator;
+    }
+}
+
---
+++ Homework7Drugs/Fraction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction(15,25);
+            Fraction fraction2 = new Fraction(6, 42);
+            Console.WriteLine(fraction1.Addition(fraction1));
+            Console.WriteLine(fraction1.Subtraction(fraction1));
+            //ffraction1.Division(fraction1);
+            //double fraction3 = (double)fraction1.Division(fraction1);
+            Console.WriteLine(fraction1.Division(fraction1));
+            Console.WriteLine(fraction1.Multiplication(fraction1));
+
+            //метод нельзя вызвать соед образом, тк
+            //он принимает параметры? fraction1.Multiplication(fraction1);
+
+        }
+    }
+}
