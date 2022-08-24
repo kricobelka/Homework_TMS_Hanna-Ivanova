04f323b33a4dc506f4b338c2095a5e32977c26d4
Hanna Ivanova <hijikataanya@gmail.com>
2022-08-24 15:44:56 +0300

    Exceptions

---
+++ Homework11Exceptions/Homework11Exceptions/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework11Exceptions
+{
+    internal class Employee
+    {
+        private int _age;
+        private string _name;
+        private double _salary;
+        public int Age
+        {
+            get => _age;
+
+            set
+            {
+                if (value > 0 && value >= 18)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new Exception($"Error. The age {value} is less than 18");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+
+            set
+            {
+                if ((value != null || value != "") && value.Length <= 100)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Error. The {value} is more than 100 symbols");
+                    //throw new ArgumentNullException($"The value {value} is null or empry");
+                    throw new ArgumentNullException(value);
+                }
+            }
+        }
+
+        public double Salary
+        {
+            get => _salary;
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Error. The {value} is less than 0 or equal to 0");
+                }
+
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+        //если я напишу argumentnullexception и argumentexception, будет отлавливать обе ошибки по порядку?
+        // если написала просто Exception, отловит также все ошибки? АНалогично первому вводу?
+    }
+}
+    
---
+++ Homework11Exceptions/Homework11Exceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework11Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Employee employee = new Employee();
+                employee.Name = @"hek";
+                //employee.Name = @"Ahelesgingfjnksjndksnlksnflksdmlksmskl
+                //                nfldksnflskdmfldskmfldksmffdsfdsfdsf
+                //                    dsfdsfdsfdsfdksnfkldfdsssssfdsfd
+                //                        kfdsffdsfrfrlljjkhtrkkl";
+                //employee.Age = 16;
+                employee.Salary = -7;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+        }
+    }
+}
