aa3258bf19bc8c24d137f90c46055daca652bfcf
Hanna Ivanova <hijikataanya@gmail.com>
2022-09-02 09:04:00 +0300

    BookWebApp

---
+++ WebApplicationBook/WebApplication1/Controllers/BookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            var book1 = new Book();
+            book1.Title = "Atlas Shrugged";
+            book1.Author = "Ayn Rand";
+            book1.NumberOfPages = 1168;
+
+            return View(book1);
+        }
+    }
+}
---
+++ WebApplicationBook/WebApplication1/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+}
