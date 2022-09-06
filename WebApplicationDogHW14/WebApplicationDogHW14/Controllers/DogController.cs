de77f45684df81fd22dc35ab8b4c6022bc357395
Hanna Ivanova <hijikataanya@gmail.com>
2022-09-07 00:06:27 +0300

    DogHW

---
+++ WebApplicationDogHW14/WebApplicationDogHW14/Controllers/DogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationDogHW14.Models;
+
+namespace WebApplicationDogHW14.Controllers
+{
+    public class DogController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IActionResult VetApplication ()
+        {
+            ModelState.Clear();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VetApplication (DogViewModel dogViewModel)
+        {
+            System.Console.WriteLine($"Name of the dog: {dogViewModel.NameOfDog}, " +
+                $"owner of the dog: {dogViewModel.NameOfOwner}, age of the dog: {dogViewModel.AgeOfDog}," +
+                $"weight of the dog: {dogViewModel.WeightOfDog}.");
+            return VetApplication();
+        }
+
+
+    }
+}
---
+++ WebApplicationDogHW14/WebApplicationDogHW14/Models/DogViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplicationDogHW14.Models
+{
+    public class DogViewModel
+    {
+        public string NameOfDog { get; set; }
+        public string NameOfOwner { get; set; }
+        public int AgeOfDog { get; set; }
+        public double WeightOfDog { get; set; }
+    }
+}
