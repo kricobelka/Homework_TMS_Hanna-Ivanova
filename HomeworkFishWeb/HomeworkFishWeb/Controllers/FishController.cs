0b5db00fa12eb6cd6f9755cde9d872ed964a6688
Hanna Ivanova <hijikataanya@gmail.com>
2022-09-11 15:59:15 +0300

    FishMoework

---
+++ HomeworkFishWeb/HomeworkFishWeb/Controllers/FishController.cs
@@ -0,0 +1,55 @@
+﻿using HomeworkFishWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkFishWeb.Controllers
+{
+    public class FishController : Controller
+    {
+        //Fish fish = new Fish[]
+        private static readonly Fish[] fish = new Fish[]
+        {
+            new Fish
+            {
+                Name = "GoldFish",
+                Weight = "7g",
+                Speed = "5km/h"
+            },
+            new Fish
+            {
+                Name = "Perch",
+                Weight = "420g",
+                Speed = "24km/h"
+            },
+            new Fish
+            {
+                Name = "Barracuda",
+                Weight = "340g",
+               Speed = "28km/h"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DataTransferByViewData()
+        {
+            ViewData["FishInformation"] = fish;
+            return View();
+        }
+
+        public IActionResult DataTransferByViewBag()
+        {
+            ViewBag.FishInformation = fish;
+            return View();
+        }
+
+        public IActionResult DataTransferByModel()
+        {
+            ViewData["Carp"] = "Carp";
+            ViewBag.Ten = "10kg";
+            return View(fish);
+        }
+    }
+}
---
+++ HomeworkFishWeb/HomeworkFishWeb/Models/Fish.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeworkFishWeb.Models
+{
+    public class Fish
+    {
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public string Speed { get; set; }
+    }
+}
